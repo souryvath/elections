a37d32a87fa497992bb4fe6ce53f4a9abdc88d90
Souryvath NIRASAY <souryvath_nirasay@carrefour.com>
2022-02-18 17:16:54 +0100

    fix

--- apps/sample/src/environments/environment.prod.ts
+++ apps/sample/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:3342'
+  api: 'http://195.154.90.2:3342'
 };
--- apps/sample/src/environments/environment.ts
+++ apps/sample/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:3342'
+  api: 'http://195.154.90.2:3342'
 };
 
 /*
