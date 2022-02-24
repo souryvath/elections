ec6b7443236449beebe7d2ad4df4bf25e10518bc
Souryvath NIRASAY <souryvath_nirasay@carrefour.com>
2022-02-24 17:55:04 +0100

    wip

--- apps/sample/src/app/app.component.ts
+++ apps/sample/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { Router } from '@angular/router';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faFacebook, faInstagram, faLinkedin, faPinterest, faTiktok, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { library, IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent {
     library: FaIconLibrary,
     @Inject(PLATFORM_ID) private readonly platformId: any,
     public router: Router) {
-    library.addIcons(faFacebook, faTwitter, faInstagram, faTiktok, faLinkedin, faPinterest); //Browse icons https://fontawesome.com/v5.15/icons
+    library.addIcons(faFacebook as IconDefinition, faTwitter as IconDefinition, faInstagram as IconDefinition, faTiktok as IconDefinition, faLinkedin as IconDefinition, faPinterest as IconDefinition); //Browse icons https://fontawesome.com/v5.15/icons
     // features https://github.com/FortAwesome/angular-fontawesome/blob/master/docs/usage/features.md
   }
 }
