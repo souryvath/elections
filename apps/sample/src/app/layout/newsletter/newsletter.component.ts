7e1cf3ff536d83b60e43dfc3dee00ae2a40da2ce
Souryvath NIRASAY <souryvath_nirasay@carrefour.com>
2022-03-18 22:55:43 +0100

    wip

--- apps/sample/src/app/layout/newsletter/newsletter.component.ts
+++ apps/sample/src/app/layout/newsletter/newsletter.component.ts
@@ -15,6 +15,21 @@ export class NewsletterComponent implements AfterViewInit {
     const script = this.renderer.createElement('script');
     script.src = `https://sibforms.com/forms/end-form/build/main.js`;
     this.renderer.appendChild(document.head, script);
+    const script2 = this.renderer.createElement('script');
+    script2.innerText  = `  window.REQUIRED_CODE_ERROR_MESSAGE = 'Veuillez choisir un code pays';
+    window.LOCALE = 'fr';
+    window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "Les informations que vous avez fournies ne sont pas valides. Veuillez vérifier le format du champ et réessayer.";
+    window.REQUIRED_ERROR_MESSAGE = "Vous devez renseigner ce champ. ";
+    window.GENERIC_INVALID_MESSAGE = "Les informations que vous avez fournies ne sont pas valides. Veuillez vérifier le format du champ et réessayer.";
+    window.translation = {
+      common: {
+        selectedList: '{quantity} liste sélectionnée',
+        selectedLists: '{quantity} listes sélectionnées'
+      }
+    };
+
+    var AUTOHIDE = Boolean(0);`;
+    this.renderer.appendChild(document.head, script2);
   }
 
 }
