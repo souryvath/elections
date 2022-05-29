9a408f3bad5970e5f7b80b4e7819e61b79fd0256
Souryvath NIRASAY <souryvath_nirasay@carrefour.com>
2022-05-29 13:17:38 +0200

    presidentielle

--- apps/sample/src/environments/environment.prod.ts
+++ apps/sample/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // api: 'https://www.les-elections.fr',
-  api: 'http://localhost:3342'
+  api: 'https://www.les-elections.fr',
+  // api: 'http://localhost:3342'
 };
